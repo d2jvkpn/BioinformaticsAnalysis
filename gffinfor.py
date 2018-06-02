a0956a762c025ebb6e166f44c9e07b75dbf2006f
Chen Bin <chenbin2018@protonmail.com>
2018-06-02 09:59:33 +0800

    Update gffinfor.py

--- gffinfor.py
+++ gffinfor.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 __author__ = 'd2jvkpn'
-__version__ = '0.4'
+__version__ = '0.5'
 __release__ = '2018-05-27'
 __project__ = 'https://github.com/d2jvkpn/GenomicProcess'
 __lisence__ = 'GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)'
@@ -11,7 +11,8 @@ from collections import defaultdict
 
 if len(os.sys.argv) != 4 or os.sys.argv[1] in ['-h', '--help']: 
     print ('Extract attribution(s) of feature(s) from gtf or gff3, usage:')
-    print ('    python3 gffinfor.py  <input.gtf/input.gff3>  <feature1,feature2...>  <attribution1,attribution2...>')
+    _ = '<feature1,feature2...>  <attribution1,attribution2...>'
+    print ('    python3 gffinfor.py  <input.gtf/input.gff3>' + _  )
     print('\nOutput: stdout, tsv format.')
 
     _ = '\nauthor: {}\nversion: {}\nrelease: {}\nproject: {}\nlisence: {}\n'
@@ -19,18 +20,12 @@ if len(os.sys.argv) != 4 or os.sys.argv[1] in ['-h', '--help']:
     print (_.format (*__))
     os.sys.exit(0)
 
-
 gtf = os.sys.argv[1]
 fea = os.sys.argv[2].split(',')
 att = os.sys.argv[3].split(',')
 
-# fea = ['transcript', 'CDS']
-# att = ['transcript_id', 'transcript_name', 'gene_id', 'gene_name', 'product']
-
 GTF = gzip.open(gtf, 'rb') if gtf.endswith('gz') else open(gtf, 'r')
-
-# print ('\t'.join(['position', 'feature'] + att))
-print ('\t'.join(att))
+print ('\t'.join (att))
 
 for _ in GTF:
     if gtf.endswith('gz'):
@@ -40,18 +35,12 @@ for _ in GTF:
 
     if fd[0].startswith('#') or len(fd) != 9 or fd[2] not in fea: continue
 
-    d = defaultdict(str)
+    d = defaultdict (str)
+    delimiter, sep = (' ', '; ')  if fd[8].count('; ') > 0 else ('=', ';')
 
-    if fd[8].count('; ') > 0:
-        __ = [ i.split(' ', 1) for i in fd[8].split('; ') ]
-    else:
-        __ = [ i.split('=', 1) for i in fd[8].split(';') ]
+    for _ in fd[8].split(sep):
+        i = _.split(delimiter, 1); d[i[0]] = i[1].strip('"')
 
-    for i in __: d[i[0]] = i[1].strip('"')
-
-#    __ = ':'.join([fd[0], fd[3], fd[4], fd[6]])
-#    print ('\t'.join ([ __, fd[2]] + [d[i] for i in att] ))
-    print ('\t'.join ([d[i] for i in att] ))
-
+    print (*[d[i] for i in att], sep='\t')
 
 GTF.close()
