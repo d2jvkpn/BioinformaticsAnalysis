969a0c342ff9128c45fc37db10790296eaa7c4f3
d2jvkpn <chenbin2018@protonmail.com>
2018-08-21 16:30:10 +0800

    Update Pathway_download.go

--- KEGG_data/Pathway_download.go
+++ KEGG_data/Pathway_download.go
@@ -27,40 +27,43 @@ func main () {
 
     var wg sync.WaitGroup
 
+    log.Printf ("%s  organism codes: %s\n", 
+    time.Now ().Format ("-0700"), strings.Join(os.Args[1:], " "))
+
     for _, v := range (os.Args[1:]) {
         ch <- true
         wg.Add (1)
-
-        go func (p string) {
-            defer func () { <- ch }()
-            defer wg.Done ()
-            log.Printf ("%s  Quering %s...\n", time.Now ().Format ("-0700"), p)
-
-            resp, err := http.Get (fmt.Sprintf (url, p))
-            if err != nil { log.Println (err); return }
-            defer resp.Body.Close ()
-
-            body, err := ioutil.ReadAll (resp.Body)
-            if err != nil { log.Println (err); return }
-
-            lines := strings.Split (string (body), "\n")
-            if ! strings.HasPrefix (lines[len (lines)-2], "#Last updated:") {
-                log.Printf ("%s  Failed to get %s\n", time.Now ().Format ("-0700"), p)
-                return
-            }
-
-            file, err := os.Create (p + ".gz")
-            if err != nil { log.Println (err); return }
-            defer file.Close ()
-
-            gw := gzip.NewWriter (file)
-            gw.Write (body)
-            gw.Close ()
-
-            log.Printf ("%s  Saved %s...\n", time.Now ().Format ("-0700"), p)
-
-        } (v + "00001.keg")
+        go Querykeg (v + "00001.keg", ch, &wg)
     }
 
     wg.Wait ()
+}
+
+func Querykeg (p string, ch <- chan bool, wg *sync.WaitGroup) {
+    defer func () { <- ch }()
+    defer wg.Done ()
+    log.Printf ("%s  Quering %s...\n", time.Now ().Format ("-0700"), p)
+
+    resp, err := http.Get (fmt.Sprintf (url, p))
+    if err != nil { log.Println (err); return }
+    defer resp.Body.Close ()
+
+    body, err := ioutil.ReadAll (resp.Body)
+    if err != nil { log.Println (err); return }
+
+    lines := strings.Split (string (body), "\n")
+    if ! strings.HasPrefix (lines[len (lines)-2], "#Last updated:") {
+        log.Printf ("%s  Failed to get %s\n", time.Now ().Format ("-0700"), p)
+        return
+    }
+
+    file, err := os.Create (p + ".gz")
+    if err != nil { log.Println (err); return }
+    defer file.Close ()
+
+    gw := gzip.NewWriter (file)
+    gw.Write (body)
+    gw.Close ()
+
+    log.Printf ("%s  Saved %s...\n", time.Now ().Format ("-0700"), p)
 }
