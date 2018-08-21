40eb562b45d8c05705fb8429014cc13f5e00eedf
d2jvkpn <chenbin2018@protonmail.com>
2018-08-21 16:42:45 +0800

    Update NCBI_gff2gtf.py

--- Gff/NCBI_gff2gtf.py
+++ Gff/NCBI_gff2gtf.py
@@ -1,8 +1,8 @@
 #! python3
 
 __author__ = 'd2jvkpn'
-__version__ = '1.3'
-__release__ = '2018-07-09'
+__version__ = '1.5'
+__release__ = '2018-08-21'
 __project__ = 'https://github.com/d2jvkpn/BioinformaticsAnalysis'
 __lisence__ = 'GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)'
 
