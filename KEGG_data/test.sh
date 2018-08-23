1ce8c2fb34a22b6ceff0db3201706d6e18f94757
d2jvkpn <chenbin2018@protonmail.com>
2018-08-23 12:16:42 +0800

    Update test.sh

--- KEGG_data/test.sh
+++ KEGG_data/test.sh
@@ -1,9 +1,13 @@
+
 mkdir example
 
-go run Pathway_download.go hsa
+code=hsa
 
-mv hsa00001.keg.gz example/
+go run Pathway_download.go $code
 
-python3 Pathway_keg2tsv.py example/hsa00001.keg.gz example/hsa00001
+mv ${code}00001.keg.gz example/
 
-Rscript pathway_classfication.r example/hsa00001.classfication.tsv example/hsa00001.classfication.pdf
+python3 Pathway_keg2tsv.py example/${code}00001.keg.gz example/${code}00001
+
+Rscript pathway_classfication.r example/${code}00001.classfication.tsv \
+example/${code}00001.classfication.pdf
