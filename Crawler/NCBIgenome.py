d3519f9374a3aac4a70468be9b4de3825edc9bac
d2jvkpn <chenbin2018@protonmail.com>
2018-08-23 21:51:35 +0800

    Update NCBIgenome.py

--- Crawler/NCBIgenome.py
+++ Crawler/NCBIgenome.py
@@ -1,4 +1,4 @@
-#! /bin/env python3
+#! /usr/bin/python3
 
 __author__ = 'd2jvkpn'
 __version__ = '0.2'
@@ -91,7 +91,7 @@ def getftp(url):
     SH.write('## Species: %s\n## Access time: %s\n\n' % (ln[-1], at))
     SH.write('prefix=%s\n\n' % prefix)
     k=0
-    wget = 'wget -c -O {0} -c $prefix/{1} -o {0}.download.log &\n'
+    wget = 'wget -c -O $(dirname $0)/{0} -c $prefix/{1} -o {0}.download.log &\n'
     
     for i in ftp:
         k += 1
