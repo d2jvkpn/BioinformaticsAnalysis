62754bcb55ad27db4614ae3b1ad24438ffc8f0e3
d2jvkpn <chenbin2018@protonmail.com>
2018-08-27 23:14:58 +0800

    Update Pathway_download.go

--- KEGG_data/Pathway_download.go
+++ KEGG_data/Pathway_download.go
@@ -12,7 +12,7 @@ import (
     "net/http"
 )
 
-const url = "http://www.kegg.jp/kegg-bin/download_htext?htext=%s&format=htext&filedir="
+const URL = "http://www.kegg.jp/kegg-bin/download_htext?htext=%s&format=htext&filedir="
 
 const HELP = `
 Archive KEGG pathway keg file by provide organism code(s), e.g. hsa mmu.
@@ -46,7 +46,7 @@ func Querykeg (p string, ch <- chan bool, wg *sync.WaitGroup) {
     defer wg.Done ()
     log.Printf ("%s  Querying %s...\n", time.Now ().Format ("-0700"), p)
 
-    resp, err := http.Get (fmt.Sprintf (url, p))
+    resp, err := http.Get (fmt.Sprintf (URL, p))
     if err != nil { log.Println (err); return }
     defer resp.Body.Close ()
 
