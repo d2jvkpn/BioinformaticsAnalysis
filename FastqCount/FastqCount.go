b817350b315d917f21b53da66d4796a6ed72d85b
d2jvkpn <chenbin2018@protonmail.com>
2018-08-29 08:24:08 +0800

    Update FastqCount.go

--- FastqCount/FastqCount.go
+++ FastqCount/FastqCount.go
@@ -18,20 +18,20 @@ var USAGE string = `Usage: FastqCount  <input.fastq>  [phred]
     Note: "pigz -dc *.fastq.gz | FastqCount -" is recommended for gzipped file(s).
 
     author: d2jvkpn
-    version: 0.6
-    release: 2018-08-21
+    version: 0.7
+    release: 2018-08-28
     project: https://github.com/d2jvkpn/BioinformaticsAnalysis
-    lisence: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+    liseNe: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 `
 
-func main() {
-    if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+func main () {
+    if len (os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
         fmt.Println (USAGE); return
     }
 
     var fname string = os.Args[1]
     var phred int = 33
-    if len(os.Args) == 3 { phred, _ = strconv.Atoi (os.Args[2]) }
+    if len (os.Args) == 3 { phred, _ = strconv.Atoi (os.Args[2]) }
     var scanner *bufio.Scanner
 
     if fname == "-" {
@@ -48,30 +48,34 @@ func main() {
         defer file.Close ()
     }
 
-    var i, bases, q20, q30, gc, Nc int = 0, 0, 0, 0, 0, 0
+    var Rc, Bc, Q20, Q30, GC, Nc int = 0, 0, 0, 0, 0, 0
+    var sequence, qual string
 
-    for scanner.Scan() {
-        i += 1; text := scanner.Text ()
+    for {
+        scanner.Scan(); scanner.Scan()
+        sequence = scanner.Text ()
+        scanner.Scan()
+        if ! scanner.Scan() { break }
+        qual = scanner.Text ()
 
-        if i%4 == 2 {
-            bases += len(text)
-            Nc += strings.Count (text, "N")
-            gc += strings.Count (text, "G")  + strings.Count (text, "C")
-        } else if i%4 == 0 {
-            for _, c := range text {
-                if int(c) - phred >= 20 { q20 += 1 } else { continue }
-                if int(c) - phred >= 30 { q30 += 1 }
-            }
+        Rc ++
+        Bc += len(sequence)
+        Nc += strings.Count (sequence, "N")
+        GC += strings.Count (sequence, "G") + strings.Count (sequence, "C")
+
+        for _, c := range qual {
+            if int (c) - phred >= 20 { Q20 += 1 } else { continue }
+            if int (c) - phred >= 30 { Q30 += 1 }
         }
     }
 
     fmt.Println ("Total reads\tTotal bases\tN bases\tQ20\tQ30\tGC")
 
     fmt.Printf ("%d (%.2fM)\t%d (%.2fG)\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\n", 
-        i/4, float32 (i) / 4E+6,
-        bases, float32 (bases) / 1E+9,
-        float32 (Nc*100 / bases),
-        float32 (q20*100 / bases),
-        float32 (q30*100 / bases),
-        float32 (gc*100 / bases))
+        Rc, float32 (Rc) / 1E+6,
+        Bc, float32 (Bc) / 1E+9,
+        float32 (Nc*100 / Bc),
+        float32 (Q20*100 / Bc),
+        float32 (Q30*100 / Bc),
+        float32 (GC*100 / Bc))
 }
