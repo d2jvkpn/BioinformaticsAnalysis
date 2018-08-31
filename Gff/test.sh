35b1fc0281bd91a9304b47959cc0ee4676a2efe9
d2jvkpn <chenbin2018@protonmail.com>
2018-08-31 09:24:11 +0800

    Update test.sh

--- Gff/test.sh
+++ Gff/test.sh
@@ -1,3 +1,5 @@
 python3 NCBI_gff2gtf_v1.7.py GCF_000001405.38_GRCh38.p12_genomic.gff.gz raw_gene
 
 python3 NCBI_gtf4stringtie.py raw_gene.gtf.gz > gene.gtf
+
+awk 'BEGIN{FS=OFS="\t"} $9~"transcript_biotype \"mRNA\""{print}' gene.gtf > mRNA.gtf
