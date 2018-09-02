61f55bab599fa1aa0ca6262304f72cce6430d0c0
d2jvkpn <chenbin2018@protonmail.com>
2018-09-02 23:24:42 +0800

    Update Pathway_initialize.py

--- KEGG_data/Pathway_initialize.py
+++ KEGG_data/Pathway_initialize.py
@@ -55,7 +55,7 @@ cmd2 = '''
 awk 'BEGIN{FS=OFS="\t"} {split($3,x,"[()]"); gsub(";", "; ", $4);
 sub(" $", "", x[1]); print $1,$2,x[1],x[2],$4}' $SP/data/meta_organism.tsv |
 awk 'BEGIN{FS=OFS="\t"; print "Entry", "code", "Scientific_name", "txid", \
-"Common_name", " Taxonomic_classification"} NR==FNR{a[$1]=$2; next}
+"Common_name", "Taxonomic_classification"} NR==FNR{a[$1]=$2; next}
 {$3=$3"\t"a[$3]; print}' $SP/data/species2txid.tsv - > KEGG_organism.tsv &&
 mv KEGG_organism.tsv $SP/data/
 '''
