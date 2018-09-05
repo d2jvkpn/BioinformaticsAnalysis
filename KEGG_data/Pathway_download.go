f9f3ea50bd82508afc503b5f7fb77179cd643a34
d2jvkpn <chenbin2018@protonmail.com>
2018-09-05 21:57:21 +0800

    Update Pathway_download.go

--- KEGG_data/Pathway_download.go
+++ KEGG_data/Pathway_download.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "log"
-    "io/ioutil"
-    "strings"
-    "time"
-    "sync"
-    "compress/gzip"
-    "net/http"
+	"os"
+	"fmt"
+	"log"
+	"io/ioutil"
+	"strings"
+	"time"
+	"sync"
+	"compress/gzip"
+	"net/http"
 )
 
 const URL = "http://www.kegg.jp/kegg-bin/download_htext?htext=%s&format=htext&filedir="
@@ -19,62 +19,62 @@ Download KEGG pathway keg file by provide organism code(s), e.g. hsa mmu.
 $ Pathway_download hsa mmu ath
 
 author: d2jvkpn
-version: 0.5
-release: 2018-09-02
+version: 0.6
+release: 2018-09-05
 project: https://github.com/d2jvkpn/BioinformaticsAnalysis
 lisense: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 `
 
 func main () {
-    if len (os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-        fmt.Println (HELP); return
-    }
+	if len (os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println (HELP); return
+	}
 
-    num := 10
-    if num > len (os.Args) - 1 { num = len (os.Args) - 1 }
-    ch := make (chan struct{}, num)
+	num := len (os.Args) - 1
+	if num > 10 { num = 10 }
+	ch := make (chan struct{}, num)
+	var wg sync.WaitGroup
 
-    var wg sync.WaitGroup
+	log.Printf ("%s  Request organism code(s):\n    %s\n", 
+		time.Now ().Format ("-0700"), strings.Join (os.Args[1:], " "))
 
-    log.Printf ("%s Request organism code(s):\n    %s\n", 
-    time.Now ().Format ("-0700"), strings.Join (os.Args[1:], " "))
+	for _, v := range (os.Args[1:]) {
+		ch <- struct{}{}
+		wg.Add (1)
+		go Querykeg (v + "00001.keg", ch, &wg)
+	}
 
-    for _, v := range (os.Args[1:]) {
-        ch <- struct{}{}
-        wg.Add (1)
-        go Querykeg (v + "00001.keg", ch, &wg)
-    }
-
-    wg.Wait ()
+	wg.Wait ()
 }
 
 func Querykeg (p string, ch <- chan struct{}, wg *sync.WaitGroup) {
-    defer func () { <- ch }()
-    defer wg.Done ()
+	defer func () { <- ch }()
+	defer wg.Done ()
 
-    log.Printf ("Querying %s...\n", p)
+	// log.Printf ("Querying %s...\n", p)
 
-    resp, err := http.Get (fmt.Sprintf (URL, p))
-    if err != nil { log.Println (err); return }
-    defer resp.Body.Close ()
+	resp, err := http.Get (fmt.Sprintf (URL, p))
+	if err != nil { log.Println (err); return }
+	defer resp.Body.Close ()
 
-    body, err := ioutil.ReadAll (resp.Body)
-    if err != nil { log.Println (err); return }
+	body, err := ioutil.ReadAll (resp.Body)
+	if err != nil { log.Println (err); return }
 
-    lines := strings.Split (string (body), "\n")
-    if ! strings.HasPrefix (lines[len (lines)-2], "#Last updated:") {
-        log.Printf ("Failed to get %s\n", p)
-        return
-    }
+	lines := strings.Split (string (body), "\n")
 
-    file, err := os.Create (p + ".gz")
-    if err != nil { log.Println (err); return }
-    defer file.Close ()
+	if ! strings.HasPrefix (lines[len (lines)-2], "#Last updated:") {
+		log.Printf (" Failed to get %s\n", p)
+		return
+	}
 
-    gw := gzip.NewWriter (file)
-    gw.Write (body)
-    gw.Close ()
+	file, err := os.Create (p + ".gz")
+	if err != nil { log.Println (err); return }
+	defer file.Close ()
 
-    log.Printf ("Saved %s...\n", p)
-    return
+	gw := gzip.NewWriter (file)
+	gw.Write (body)
+	gw.Close ()
+
+	log.Printf (" Saved %s...\n", p)
+	return
 }
