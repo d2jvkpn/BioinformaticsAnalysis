31fd7994cc2322d6eeedbca202d71a8f926cd8e9
d2jvkpn <chenbin2018@protonmail.com>
2018-09-08 19:38:37 +0800

    Update test.sh

--- KEGG_data/test.sh
+++ KEGG_data/test.sh
@@ -1,16 +1,16 @@
 # Update local data table for command "match":
-PathwayTK  update
+Pathway  update
 
 # Find match species in local data table:
-PathwayTK  match  "Rhinopithecus roxellana"
-PathwayTK  match  Rhinopithecus+roxellana
+Pathway  match  "Rhinopithecus roxellana"
+Pathway  match  Rhinopithecus+roxellana
 
 # Get organisms keg file:
-PathwayTK  get  hsa mmu ath
+Pathway  get  hsa mmu ath
 
 # Convert keg format to tsv:
-PathwayTK  totsv  hsa00001.keg.gz  hsa00001.keg.tsv
-PathwayTK  totsv  hsa00001.keg  hsa00001.keg.tsv
+Pathway  totsv  hsa00001.keg.gz  hsa00001.keg.tsv
+Pathway  totsv  hsa00001.keg  hsa00001.keg.tsv
 
 # Get species keg and convert to tsv:
-PathwayTK  species  Rhinopithecus+roxellana
+Pathway  species  Rhinopithecus+roxellana
