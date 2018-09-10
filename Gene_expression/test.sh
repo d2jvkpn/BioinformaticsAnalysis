4beb41de8ab64e0d3e15e5feae2f9708fb706302
d2jvkpn <chenbin2018@protonmail.com>
2018-09-10 21:18:15 +0800

    Update test.sh

--- Gene_expression/test.sh
+++ Gene_expression/test.sh
@@ -1,10 +1,9 @@
-
-Rscript gene_expression_singles.r example/gene_TPM.tsv example/singles/gene_TPM 0.01 TPM
-
-sh merge_corheatmap.sh example/singles/gene_TPM
-
-
 Rscript gene_expression_group.r example/gene_TPM.tsv example/group/gene_TPM 0.01 \
 TPM example/group.tsv
 
 sh merge_corheatmap.sh example/group/gene_TPM
+
+
+Rscript gene_expression_singles.r example/gene_TPM.tsv example/singles/gene_TPM 0.01 TPM
+
+sh merge_corheatmap.sh example/singles/gene_TPM
