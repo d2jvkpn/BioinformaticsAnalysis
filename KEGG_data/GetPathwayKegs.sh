184e06e8dfe32f517c8ce3f3bf640b7a04e768f7
d2jvkpn <chenbin2018@protonmail.com>
2018-09-12 13:12:06 +0800

    Create GetPathwayKegs.sh

---
+++ KEGG_data/GetPathwayKegs.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -eu -o pipefail
+
+wd=$PWD
+dp=$(dirname $(which Pathway))
+
+cd $dp
+
+awk 'NR>1{print $2}' KEGG_organism.tsv |
+xargs -i -n 100 Pathway Get &> get_pathway.log
+
+awk '/Failed/{print $NF}' get_pathway.log > get_pathway.failed
+
+mkdir Pathway_kegs && mv *.keg.gz Pathway_kegs/
+
+cd $wd
