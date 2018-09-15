6dba856c885eb47fd769b65454e1cbf4715b8723
d2jvkpn <chenbin2018@protonmail.com>
2018-09-15 10:52:29 +0800

    Create kegtsv2mapper.sh

---
+++ KEGG/kegtsv2mapper.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -eu -o pipefail
+
+tsv=$1
+
+awk 'BEGIN{FS=OFS="\t"} $2~"^PATH:"{a[$1]=$2; b[$1]=$3"\t"$7"\t"$9; next}
+a[$1] {sub("PATH:", "", a[$1]); split($5,x,"; ");
+if(length(x)==2) {g=x[1]} else {g=""};
+split($1, x, " "); print $4, a[$1], g, x[1], $11, b[$1]}' $tsv
