1530627ee09588a5c416c39ee4a4aa16b8259388
d2jvkpn <chenbin2018@protonmail.com>
2018-09-16 10:52:15 +0800

    Update and rename Genome_crawler/test.sh to Ensembl/test.sh

--- Ensembl/test.sh
+++ Ensembl/test.sh
@@ -1,16 +1,3 @@
-
-python3 NCBIgenome_v0.4.py search "Homo sapiens"
-python3 NCBIgenome_v0.4.py search Homo+sapiens
-# https://www.ncbi.nlm.nih.gov/genome/?term=Homo+sapiens%5Borgn%5D
-
-python3 NCBIgenome_v0.4.py getftp https://www.ncbi.nlm.nih.gov/genome/?term=Homo+sapiens%5Borgn%5D
-# created NCBI__Homo_sapiens__GCF_000001405.38_GRCh38.p12/{download.sh,genome.infor.txt}
-
-sh NCBI__Homo_sapiens__GCF_000001405.38_GRCh38.p12/download.sh
-# download reference genome to NCBI__Homo_sapiens__GCF_000001405.38_GRCh38.p12/
-
-
-
 python3 EnsemblGenome_v0.7.py search "Homo sapiens"
 python3 EnsemblGenome_v0.7.py search Homo+sapiens
 # http://asia.ensembl.org/Homo_sapiens/Info/Index
