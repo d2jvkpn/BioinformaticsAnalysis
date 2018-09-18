db2fe92998015537c4442961425c4607363e5e29
d2jvkpn <chenbin2018@protonmail.com>
2018-09-18 22:43:20 +0800

    Update Gene2Pathway_mapper.sh

--- KEGG/Gene2Pathway_mapper.sh
+++ KEGG/Gene2Pathway_mapper.sh
@@ -4,8 +4,8 @@ set -eu -o pipefail
 
 tsv=$1
 
-awk 'BEGIN{FS=OFS="\t"} $2~"^PATH:"{a[$1]=$2; b[$1]=$3"\t"$7"\t"$9; next}
+cat  $tsv | awk 'BEGIN{FS=OFS="\t"} $2~"^PATH:"{a[$1]=$2; b[$1]=$3"\t"$7"\t"$9; next}
 a[$1] {sub("PATH:", "", a[$1]); split($5,x,"; ");
 if(length(x)==2) {g=x[1]} else {g=""};
 sub("[[]EC:", "", $11); sub("[]]", "", $11);
-split($10, x, " "); print $4, a[$1], g, x[1], $11, b[$1]}' $tsv
+split($10, x, " "); print $4, a[$1], g, x[1], $11, b[$1]}'
