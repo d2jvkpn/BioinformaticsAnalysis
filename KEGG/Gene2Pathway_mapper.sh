a7b8eeab69e2edbb5fb6653b80f6ada82e17f6b4
d2jvkpn <chenbin2018@protonmail.com>
2018-09-20 00:05:18 +0800

    Update Gene2Pathway_mapper.sh

--- KEGG/Gene2Pathway_mapper.sh
+++ KEGG/Gene2Pathway_mapper.sh
@@ -4,8 +4,11 @@ set -eu -o pipefail
 
 tsv=$1
 
-cat  $tsv | awk 'BEGIN{FS=OFS="\t"} $2~"^PATH:"{a[$1]=$2; b[$1]=$3"\t"$7"\t"$9; next}
-a[$1] {sub("PATH:", "", a[$1]); split($5,x,"; ");
+cat $tsv | awk 'BEGIN{FS=OFS="\t"}
+$1~"^#" && $2~"^PATH:"{sub("#", "", $1); a[$1]=$2;
+  sub(".*:", "", $4); sub(".*:", "", $5);
+  b[$1]=$3"\t"$4"\t"$5; next}
+a[$1] {sub("PATH:", "", a[$1]); split($3,x,"; ");
 if(length(x)==2) {g=x[1]} else {g=""};
-sub("[[]EC:", "", $11); sub("[]]", "", $11);
-split($10, x, " "); print $4, a[$1], g, x[1], $11, b[$1]}'
+sub("[[]EC:", "", $4); sub("[]]", "", $4);
+split($4, x, " "); print $2, a[$1], g, x[1], $5, b[$1]}'
