b3289a1adf6f9ac8d0afd6366da86eb4a492df8b
d2jvkpn <chenbin2018@protonmail.com>
2018-09-26 16:38:40 +0800

    Update GetAllPathwayKegs.sh

--- KEGG/GetAllPathwayKegs.sh
+++ KEGG/GetAllPathwayKegs.sh
@@ -18,9 +18,13 @@ for i in $(ls *.keg.gz); do
     awk -v i=$(basename $i | sed 's/[0-9]*.keg.gz//') 'BEGIN{FS=OFS="\t"}
     {sub("PATH:", "", $1); sub("[]]", "", $1); sub(i, "ko", $1); print i, $1}'
 done |
-awk 'BEGIN{FS=OFS="\t"; print "Lineage", "PATH"} NR==FNR{if(NR>1) a[$2]=$4; next}
+awk 'BEGIN{FS=OFS="\t"} NR==FNR{if(NR>1) a[$2]=$4; next}
 a[$1]{if(++x[a[$1]"\t"$2]) print a[$1], $2}' KEGG_organism.tsv - |
-sort | gzip -c > Lineage_PATH.tsv.gz
+sort | sed '1i Lineage\tPATH' | gzip -c > Lineage_PATH.tsv.gz
+
+gunzip -c Lineage_PATH.tsv.gz | awk 'BEGIN{FS=OFS="\t"}
+NR==1{print} NR>1{split($1,x,";");
+if (++y[x[2]"\t"$2]==1) print x[2],$2}' > LineageL2_PATH.tsv
 
 tar -cf Pathway_keg.tar *.keg.gz
 rm *.keg.gz
