c46389211052dedb42ebd1586dae003ea7b7a284
d2jvkpn <chenbin2018@protonmail.com>
2018-09-28 16:35:38 +0800

    Update FastqCount.go

--- FastqCount/FastqCount.go
+++ FastqCount/FastqCount.go
@@ -1,100 +1,125 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "log"
-    "strings"
-    "strconv"
-    "runtime"
-    gzip "github.com/klauspost/pgzip" //"compress/gzip"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"flag"
+	"path/filepath"
+	"runtime"
+	"strings"
+	gzip "github.com/klauspost/pgzip" //"compress/gzip"
 )
 
+const USAGE = `
+Usage: FastqCount  [-phred value]  [-o tsv]  < - input1.fastq input2.fastq.gz >
+  output (tsv) header: Total reads  Total bases  N bases  Q20  Q30  GC
+  note: "pigz -dc *.fastq.gz | FastqCount -" is recommended for gzipped file(s).
+`
 
-var USAGE string = `
-Usage: FastqCount  <input.fastq>  [phred]
-    output (tsv): Total reads  Total bases  N bases  Q20  Q30  GC
-    phred default: 33
-    note: "pigz -dc *.fastq.gz | FastqCount -" is recommended for gzipped file(s).
 
+const LISENSE = `
 author: d2jvkpn
-version: 0.8
-release: 2018-08-30
+version: 0.9
+release: 2018-09-27
 project: https://github.com/d2jvkpn/BioinformaticsAnalysis
 lisense: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 `
 
-func main () {
-    if len (os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-        fmt.Println (USAGE); return
-    }
+func main() {
+	output := flag.String("o", "", "output summary to a tsv file")
+	phred := flag.Int("phred", 33, "set phred value")
+	flag.Parse()
+	inputs := flag.Args()
 
-    var fname string = os.Args[1]
-    var phred int = 33
-    var err error
+	flag.Usage = func() {
+		fmt.Println (USAGE)
+		flag.PrintDefaults ()
+		fmt.Println (LISENSE)
+	}
 
-    if len (os.Args) == 3 {
-        phred, err = strconv.Atoi (os.Args[2])
-        if err != nil { log.Fatal (err) }
-    }
+	if len (inputs) == 0 { flag.Usage(); os.Exit(0) }
 
-    var scanner *bufio.Scanner
+	type Writer interface { Write(p []byte) (n int, err error) }
+	var err error
+	var wt Writer
+	ch := make(chan [2]string, 10000)
+	var Rc, Bc, Q20, Q30, GC, Nc int = 0, 0, 0, 0, 0, 0
+	runtime.GOMAXPROCS (4)
 
-    if fname == "-" {
-        scanner = bufio.NewScanner (os.Stdin)
-    } else {
-        file, err := os.Open (fname)
-        if err != nil { log.Fatal (err) }
+	go func() {
+		for _, s := range inputs {
+			scanner, frd, err := ReadInput(s)
+			if err != nil { log.Fatal(err) }
+			defer frd.Close()
 
-        if strings.HasSuffix (fname, ".gz") {
-            gz, err := gzip.NewReader (file)
-            if err != nil { log.Fatal (err) }
-            scanner = bufio.NewScanner (gz)
-        } else { scanner = bufio.NewScanner (file) }
+			for {
+				var blk [2]string
+				scanner.Scan()
+				scanner.Scan()
+				blk[0] = scanner.Text()
+				scanner.Scan()
+				if !scanner.Scan() { break }
+				blk[1] = scanner.Text()
+				ch <- blk
+			}
+		}
+		close(ch)
+	}()
 
-        defer file.Close ()
-    }
 
-    var Rc, Bc, Q20, Q30, GC, Nc int = 0, 0, 0, 0, 0, 0
-    ch := make (chan [2]string, 1000)
-    runtime.GOMAXPROCS (2)
+	for k := range ch {
+		Rc++
+		Bc += len(k[0])
+		Nc += strings.Count(k[0], "N")
+		GC += strings.Count(k[0], "GC")
 
-    go func () {
-        for {
-            var blk [2]string
-            scanner.Scan (); scanner.Scan ()
-            blk[0] = scanner.Text ()
-            scanner.Scan ()
-            if ! scanner.Scan () { break }
-            blk[1] = scanner.Text ()
-            ch <- blk
-        }
+		for _, q := range k[1] {
+			if int(q)-*phred >= 20 { Q20++ } else { continue }
+			if int(q)-*phred >= 30 { Q30++ }
+		}
+	}
 
-        close (ch)
-    } ()
+	wt = os.Stdout
 
-    for k := range ch {
-        Rc ++
-        Bc += len (k[0])
-        Nc += strings.Count (k[0], "N")
-        GC += strings.Count (k[0], "GC")
+	if *output != "" {
+		err = os.MkdirAll (filepath.Dir (*output), 0755)
+		if err != nil {
+			log.Println (err)
+		} else {
+			wt, err = os.Create (*output)
+			if err != nil { log.Println(err); wt = os.Stdout }
+		}
+	}
 
-        for _, q := range k[1] {
-            if int (q) - phred >= 20 { Q20 ++ } else { continue }
-            if int (q) - phred >= 30 { Q30 ++ }
-        }
-    }
+	fmt.Fprintln (wt, "Total reads\tTotal bases\tN bases\tQ20\tQ30\tGC")
 
-    fmt.Println ("Total reads\tTotal bases\tN bases\tQ20\tQ30\tGC")
+	fmt.Fprintf (wt, "%.2fM\t%.2fG\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\n",
+		float64(Rc) / float64(1E+6),
+		float64(Bc) / float64(1E+9),
+		float64(Nc*100) / float64(Bc),
+		float64(Q20*100) / float64(Bc),
+		float64(Q30*100) / float64(Bc),
+		float64(GC*100) / float64(Bc))
 
-    fmt.Printf ("%.2fM\t%.2fG\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\n", 
-        float64 (Rc) / float64 (1E+6),
-        float64 (Bc) / float64 (1E+9),
-        float64 (Nc*100) / float64 (Bc),
-        float64 (Q20*100) / float64 (Bc),
-        float64 (Q30*100) / float64 (Bc),
-        float64 (GC*100) / float64 (Bc))
+	fmt.Fprintf (wt, "%d\t%d\t%d\t%d\t%d\t%d\n", Rc, Bc, Nc, Q20, Q30, GC)
+}
 
-    fmt.Printf ("%d\t%d\t%d\t%d\t%d\t%d\n", Rc, Bc, Nc, Q20, Q30, GC)
+func ReadInput(s string) (scanner *bufio.Scanner, file *os.File, err error) {
+	if s == "-" { scanner = bufio.NewScanner(os.Stdin); return }
+
+	file, err = os.Open(s)
+	if err != nil { return }
+
+	if strings.HasSuffix(s, ".gz") {
+		var gz *gzip.Reader
+		gz, err = gzip.NewReader(file)
+		if err != nil { return }
+		scanner = bufio.NewScanner(gz)
+	} else {
+		scanner = bufio.NewScanner(file)
+	}
+
+	return
 }
