70b52a08e229654a74d037eba9bab721fe07c9ae
d2jvkpn <chenbin2018@protonmail.com>
2018-10-09 12:37:57 +0800

    Update cat2gz.go

--- Toolkits/cat2gz.go
+++ Toolkits/cat2gz.go
@@ -1,97 +1,98 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"strings"
 	"bufio"
-	"log"
 	"flag"
-	"path/filepath"
+	"fmt"
 	gzip "github.com/klauspost/pgzip"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const USEAGE = `
 Concatenate stdin, text files, gzipped files to one gzipped file.
 Usage:
-  $ cat2gz  <-o output.gz>  [-p cpu_max]  < input1.fastq input2.fastq.gz - >`
+  $ cat2gz  <-o output.gz>  [-p cpu_max]  <input1.fastq input2.fastq.gz>
+  note: when input is -, read standard input.`
 
 const LISENSE = `
 author: d2jvkpn
-version: 0.4
-release: 2018-09-26
+version: 0.5
+release: 2018-10-09
 project: https://github.com/d2jvkpn/BioinformaticsAnalysis
 lisense: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 `
 
-func main () {
-    outgz := flag.String ("o", "", "output gzipped file")
-    cpunum := flag.Int ("p", 4, "max cpu number to use")
-    flag.Parse ()
+func main() {
+	outgz := flag.String("o", "", "output gzipped file")
+	cpunum := flag.Int("p", 4, "max cpu number to use")
+	level := flag.Int("level", 6, "compress level")
+	flag.Parse()
 
 	inputs := flag.Args()
 
 	flag.Usage = func() {
-		fmt.Println (USEAGE)
-		flag.PrintDefaults ()
-		fmt.Println (LISENSE)
+		fmt.Println(USEAGE)
+		flag.PrintDefaults()
+		fmt.Println(LISENSE)
 	}
 
-	if len(inputs) == 0 || *outgz == "" {
-		flag.Usage ()
-		os.Exit (1)
-	}
+	if len(inputs) == 0 || *outgz == "" { flag.Usage(); os.Exit(1) }
 
 	type Writer interface {
-		Write (p []byte) (n int, err error)
+		Write(p []byte) (n int, err error)
 	}
 
 	var wt Writer
 	var err error
 
-	err = os.MkdirAll (filepath.Dir (*outgz), 0755)
-	if err != nil { log.Fatal (err)}
+	err = os.MkdirAll(filepath.Dir(*outgz), 0755)
+	if err != nil { log.Fatal(err) }
 
 	var fwt *os.File
-	
-	fwt, err = os.Create (*outgz)
-	if err != nil { log.Fatal (err)}
-	defer fwt.Close ()
 
-	gznw := gzip.NewWriter (fwt)
-	gznw.SetConcurrency (100000, *cpunum)
+	fwt, err = os.Create(*outgz)
+	if err != nil { log.Fatal(err) }
+	defer fwt.Close()
+
+	gznw, err := gzip.NewWriterLevel(fwt, *level)
+	if err != nil { log.Fatal(err) }
+	gznw.SetConcurrency(100000, *cpunum)
+
 	wt = gznw
-	defer gznw.Close ()
+	defer gznw.Close()
 
 	for _, s := range inputs {
-		scanner, frd, err := ReadInput (s)
-		if err != nil { log.Fatal (err) }
-		defer frd.Close ()
+		scanner, frd, err := ReadInput(s)
+		if err != nil { log.Fatal(err) }
+		defer frd.Close()
 
-		for scanner.Scan () {
-			_, err = wt.Write ([]byte (scanner.Text () + "\n"))
-			if err != nil { log.Fatal (err) }
+		for scanner.Scan() {
+			_, err = wt.Write([]byte(scanner.Text() + "\n"))
+			if err != nil { log.Fatal(err) }
 		}
 	}
 }
 
-func ReadInput (s string) (scanner *bufio.Scanner, file *os.File, err error) {
+func ReadInput(s string) (scanner *bufio.Scanner, file *os.File, err error) {
 	if s == "-" {
-		scanner = bufio.NewScanner (os.Stdin)
+		scanner = bufio.NewScanner(os.Stdin)
 		return
 	}
 
-	file, err = os.Open (s)
-	if err != nil { return } 
+	file, err = os.Open(s)
+	if err != nil { return }
 
-	if strings.HasSuffix (s, ".gz") {
+	if strings.HasSuffix(s, ".gz") {
 		var gz *gzip.Reader
-		gz, err = gzip.NewReader (file)
-		if err != nil { return } 
-		scanner = bufio.NewScanner (gz)
+		gz, err = gzip.NewReader(file)
+		if err != nil { return }
+		scanner = bufio.NewScanner(gz)
 	} else {
-		scanner = bufio.NewScanner (file)
+		scanner = bufio.NewScanner(file)
 	}
- 
+
 	return
 }
