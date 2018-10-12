e163f0cc23017ef020fa71ff0938e0ce87539d6e
d2jvkpn <chenbin2018@protonmail.com>
2018-10-12 15:52:49 +0800

    Delete topng.sh

--- Toolkits/topng.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# project: https://github.com/d2jvkpn/BioinformaticsAnalysis
-
-i=$1
-
-convert -density 300 -depth 8 -quality 85 $i ${i%.*}.png 2> /dev/null
-
-test $? -eq 0 && echo "OK: $i" || echo "Failed: $i"
