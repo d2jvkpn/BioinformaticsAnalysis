0439d3e04b7deb1c94894060cdf9880ed9a9bbfa
d2jvkpn <chenbin2018@protonmail.com>
2018-12-13 10:37:56 +0800

    Update and rename pHeatmap.r to Heatmap_expression.r

--- Heatmap/Heatmap_expression.r
+++ Heatmap/Heatmap_expression.r
@@ -4,7 +4,7 @@ Args <- commandArgs (T)
 if (length (Args) != 3 ) {
 	print ("Arguments:  <expression.tsv>  <output.pdf> <title>")
     print ("  read environment variables: PDFheigh, PDFwidth, COLOR1, COLOR2,")
-    print ("    COLOR3, PDFwidth, COLOR1, COLOR2, COLOR3")
+    print ("    COLOR3, PDFwidth, COLOR1, COLOR2, COLOR3, ShowRownames")
 	print ("author: d2jvkpn")
 	print ("version: 0.3")
 	print ("release: 2018-12-05")
@@ -18,11 +18,18 @@ hd <- read.delim (Args[1], check.names=FALSE, row.names=1)
 PDF <- Args[2]
 title <- Args[3]
 
-PDFheigh <- as.numeric(Sys.getenv(x="PDFheigh", unset="6"))
+PDFheigh <- as.numeric(Sys.getenv(x="PDFheigh", unset="8"))
 PDFwidth <- as.numeric(Sys.getenv(x="PDFwidth", unset="6"))
 COLOR1 <- Sys.getenv(x="COLOR1", unset="blue")
 COLOR2 <- Sys.getenv(x="COLOR2", unset="white")
 COLOR3 <- Sys.getenv(x="COLOR3", unset="white")
+ShowRownames <- Sys.getenv(x="ShowRownames", unset="")
+
+if (nrow(hd) <= 50 & ShowRownames == "") {
+	ShowRownames == "TRUE"
+}
+
+ShowRownames = as.logical(ShowRownames)
 
 hd <- hd [apply(hd,1,var) > 0,]
 
@@ -32,5 +39,5 @@ print (PDF)
 pheatmap (log (hd + 1, 10),
     scale = ifelse (ncol(hd) > 2, "row", "column"),
     breaks = NA, color = palette, cluster_rows = TRUE,
-    cluster_cols = ncol(hd) > 2, show_rownames = nrow(hd) <= 50,
+    cluster_cols = ncol(hd) > 2, show_rownames = ShowRownames,
     border_color = NA, filename = PDF, heigh = PDFheigh, width = PDFwidth)
