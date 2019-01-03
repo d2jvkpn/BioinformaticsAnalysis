02dbdd8f6d38f1d216d40aa5014eb506a9719fda
d2jvkpn <chenbin2018@protonmail.com>
2019-01-03 21:35:24 +0800

    Update Heatmap_expression.r

--- Heatmap/Heatmap_expression.r
+++ Heatmap/Heatmap_expression.r
@@ -4,10 +4,10 @@ Args <- commandArgs (T)
 if (length (Args) != 3 ) {
 	print ("Arguments:  <expression.tsv>  <output.pdf> <title>")
     print ("  read environment variables: PDFheigh, PDFwidth, COLOR1, COLOR2,")
-    print ("    COLOR3, PDFwidth, COLOR1, COLOR2, COLOR3, ShowRownames")
+    print ("    COLOR3, PDFwidth, COLOR1, COLOR2, COLOR3, ShowRownames, CLusterRows")
 	print ("author: d2jvkpn")
-	print ("version: 0.3")
-	print ("release: 2018-12-05")
+	print ("version: 0.4")
+	print ("release: 2019-01-03")
 	print ("project: https://github.com/d2jvkpn/BioinformaticsAnalysis")
 	print ("lisense: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html")
 	q ("no", 2, FALSE)
@@ -24,6 +24,7 @@ COLOR1 <- Sys.getenv(x="COLOR1", unset="blue")
 COLOR2 <- Sys.getenv(x="COLOR2", unset="white")
 COLOR3 <- Sys.getenv(x="COLOR3", unset="red")
 ShowRownames <- Sys.getenv(x="ShowRownames", unset="")
+ClusterRows <- Sys.getenv(x="ClusterRows", unset="TRUE")
 
 if (nrow(hd) <= 50 & ShowRownames == "") {
 	ShowRownames = "TRUE"
@@ -35,11 +36,18 @@ if (ShowRownames == "") {
 
 hd <- hd [apply(hd,1,var) > 0,]
 
+if (as.logical(ClusterRows) == FALSE) {
+	scale = "none"
+	nhd = hd
+} else {
+	scale = ifelse (ncol(hd) > 2, "row", "column")
+	nhd = log (hd + 1, 10)
+}
+
 palette <- colorRampPalette (c (COLOR1, COLOR2, COLOR3))(n=299)
 print (PDF)
 
-pheatmap (log (hd + 1, 10),
-    scale = ifelse (ncol(hd) > 2, "row", "column"),
-    breaks = NA, color = palette, cluster_rows = TRUE,
+pheatmap (nhd, scale = scale,
+    breaks = NA, color = palette, cluster_rows = as.logical(ClusterRows),
     cluster_cols = ncol(hd) > 2, show_rownames = as.logical(ShowRownames),
     border_color = NA, filename = PDF, heigh = PDFheigh, width = PDFwidth)
