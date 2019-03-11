df5f7738c9f2f7237af547f7952f8ab6a9bd8ee7
d2jvkpn <chenbin2018@protonmail.com>
2019-03-11 12:26:54 +0800

    Update bowtie2_reads_filter.sh

--- HowTo/bowtie2_reads_filter.sh
+++ HowTo/bowtie2_reads_filter.sh
@@ -10,10 +10,10 @@ mkdir -p log
 bowtie2 -k30 -t -p 15 -x target -1 $i.R1.fastq.gz -2 $i.R2.fastq.gz \
 --un-conc ./un-conc.$i --al-conc ./al-conc.$i -S /dev/null 2> log/bowtie2.$i.log
 
-mv ./un-conc.$i.1 un-conc_$i.R1.fastq
-mv ./un-conc.$i.2 un-conc_$i.R2.fastq
-mv ./al-conc-mate.$i.1 al-conc_$i.R1.fastq
-mv ./al-conc-mate.$i.2 al-conc_$i.R2.fastq
+mv ./un-conc.1.$i un-conc_$i.R1.fastq
+mv ./un-conc.2.$i un-conc_$i.R2.fastq
+mv ./al-conc-mate.1.$i al-conc_$i.R1.fastq
+mv ./al-conc-mate.2.$i al-conc_$i.R2.fastq
 
 pigz un-conc_$i.R1.fastq un-conc_$i.R2.fastq \
 al-conc_$i.R1.fastq al-conc_$i.R2.fastq
