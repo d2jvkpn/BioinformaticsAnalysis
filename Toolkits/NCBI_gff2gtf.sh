64826171c426ac58dd4a2058d7b00f8dbabc1651
d2jvkpn <chenbin2018@protonmail.com>
2019-06-07 14:49:39 +0800

    Update NCBI_gff2gtf.sh

--- Toolkits/NCBI_gff2gtf.sh
+++ Toolkits/NCBI_gff2gtf.sh
@@ -7,4 +7,5 @@ gffread -EFG NCBI.gff3 -T -o genomic.gtf
 
 Gffinfor genomic.gtf exon \
 transcript_id,gbkey,Name,product,gene_id,gene_biotype,gene_name |
-awk 'BEGIN{FS=OFS="\t"} ++x[$1]==1{print}' > transcript.infor.tsv
+awk 'BEGIN{FS=OFS="\t"} NR==1{$3="transcript_name"}
+++x[$1]==1{print}' > transcript.infor.tsv
