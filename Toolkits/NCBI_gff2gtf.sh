580dba40e86b2654b9a3faf84d0d97efca47fcfd
d2jvkpn <chenbin2018@protonmail.com>
2019-06-11 23:25:56 +0800

    Update NCBI_gff2gtf.sh

--- Toolkits/NCBI_gff2gtf.sh
+++ Toolkits/NCBI_gff2gtf.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+# https://github.com/d2jvkpn/BioinformaticsAnalysis
+
 set -eu -o pipefail
 
 gffread -EFG NCBI.gff3 -T -o genomic.gtf
 # missing gene and transcript records
 
 Gffinfor genomic.gtf exon \
-transcript_id,gbkey,Name,product,gene_id,gene_biotype,gene_name |
-awk 'BEGIN{FS=OFS="\t"} NR==1{$3="transcript_name"}
-++x[$1]==1{print}' > transcript.infor.tsv
+transcript_id,gbkey,Name,product,gene_id,gene_biotype,gene_name,description  \
+gbkey:transcript_biotype,Name:transcript_name,description:gene_description |
+awk 'BEGIN{FS=OFS="\t"} ++x[$1]==1{print}' > transcript.infor.tsv
