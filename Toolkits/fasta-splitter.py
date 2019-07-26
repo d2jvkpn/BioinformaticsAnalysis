86bd725da24861143ae2b86352b8deffd8cf8b93
d2jvkpn <chenbin2018@protonmail.com>
2019-07-26 23:35:36 +0800

    Update fasta-splitter.py

--- Toolkits/fasta-splitter.py
+++ Toolkits/fasta-splitter.py
@@ -30,16 +30,13 @@ q, r = divmod (m, n)
 qs = [q]*n
 for i in range(r): qs[i] +=1
 ns = [ prefix + ".part-%d.fasta" % (i+1) for i in range(n)]
-os.makedirs(os.path.dirname(os.path.abspath(prefix)), exist_ok=True)
 
-i, x = 0, 0
-sfa = SeqIO.parse(fa, "fasta")
+os.makedirs(os.path.dirname(os.path.abspath(prefix)), exist_ok=True)
+print("Split {} into {} parts...".format(fa, n))
+i, x, tmp, sfa = 0, 0, "saved {}, {} records", SeqIO.parse(fa, "fasta")
 for r in sfa:
     if i == 0: f = open(ns[x], "w")
     SeqIO.write(r, f, 'fasta'); i += 1
-    if i == qs[x]:
-         f.close()
-         print("saved {}, {} records".format(ns[x], qs[x]))
-         i, x = 0, x + 1
+    if i == qs[x]: f.close(); print(tmp.format(ns[x], qs[x])); i, x = 0, x + 1
 
 sfa.close()
