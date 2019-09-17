4a10cf4584b0f8685f824ca395c949dbf7000c19
d2jvkpn <chenbin2018@protonmail.com>
2019-09-17 22:15:19 +0800

    log to stderr

--- fasta-splitter/fasta-splitter.py
+++ fasta-splitter/fasta-splitter.py
@@ -2,26 +2,33 @@ import os
 from Bio import SeqIO
 
 __author__ = 'd2jvkpn'
-__version__ = '0.2'
-__release__ = '2019-07-26'
+__version__ = '0.3'
+__release__ = '2019-08-31'
 __project__ = 'https://github.com/d2jvkpn/BioinformaticsAnalysis'
 __lisense__ = 'GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)'
 
+HELP = '''Split fasta into parts, usage:
+  $ python3 fasta-splitter.py  <input.fasta>  <n>  <output_prefix>
+
+author: {}
+version: {}
+release: {}
+project: {}
+lisence: {}
+'''
+
 if len(os.sys.argv) != 4:
-    print("Split fasta into parts, usage:")
-    print("  $ python3 fasta-splitter.py  <input.fasta>  <n>  <output_prefix>")
-    _ = '\nauthor: {}\nversion: {}\nrelease: {}\nproject: {}\nlisence: {}\n'
-    __ = [__author__,  __version__, __release__, __project__, __lisense__]
-    print (_.format (*__))
+    _ = [__author__,  __version__, __release__, __project__, __lisense__]
+    print (HELP.format (*_), file=os.sys.stderr)
     os.sys.exit(2)
 
 fa, n, prefix = os.sys.argv[1:4]
 n = int(n)
-if n < 1: print("invalid parts number:", n); os.sys.exit(1)
+if n < 1: os.sys.exit("invalid parts number: %d" % n)
 # fa, n, prefix = "mRNA.fa", 20, "out"
 
 m = 0
-sfa = SeqIO.parse(fa, "fasta") 
+sfa = SeqIO.parse(fa, "fasta")
 for r in sfa: m+=1
 sfa.close()
 
@@ -32,11 +39,16 @@ for i in range(r): qs[i] +=1
 ns = [ prefix + ".part-%d.fasta" % (i+1) for i in range(n)]
 
 os.makedirs(os.path.dirname(os.path.abspath(prefix)), exist_ok=True)
-print("Split {} into {} parts...".format(fa, n))
+print("Split {} into {} parts...".format(fa, n), file=os.sys.stderr)
 i, x, tmp, sfa = 0, 0, "saved {}, {} records", SeqIO.parse(fa, "fasta")
 for r in sfa:
     if i == 0: f = open(ns[x], "w")
     SeqIO.write(r, f, 'fasta'); i += 1
-    if i == qs[x]: f.close(); print(tmp.format(ns[x], qs[x])); i, x = 0, x + 1
+    if i == qs[x]:
+        f.close()
+        print(tmp.format(ns[x], qs[x]), file=os.sys.stderr)
+        i, x = 0, x + 1
 
 sfa.close()
+
+print(n)
